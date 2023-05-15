1b5c7d72e7c2dc484b0df7ba16b18fbdcf2c67e1
kropf <kropf@KEVIN-ASUS>
2023-05-15 21:13:27 +0200

    Add project files.

---
+++ CQD4CS_HFT_2022232.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CQD4CS_HFT_2022232.Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ CQD4CS_HFT_2022232.Test/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CQD4CS_HFT_2022232.Test
+{
+    public class Class1
+    {
+    }
+}
