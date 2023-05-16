d7e70ce5231ef55e811bf2ff9b9769963412615d
kropfkevin11 <kropfkevin11@gmail.com>
2023-05-16 19:53:05 +0200

    implement festival class

--- CQD4CS_HFT_2022232.Models/Festival.cs
+++ CQD4CS_HFT_2022232.Models/Festival.cs
@@ -1,9 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CQD4CS_HFT_2022232.Models
 {
     public class Festival
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int Duration { get; set; }
+        [NotMapped]
+        public virtual ICollection<Artist> Artists { get; set; }
 
+        public Festival()
+        {
+            Artists = new HashSet<Artist>();
+        }
+
+        public override string ToString()
+        {
+            return $"#{Id}-FESTIVAL: Name = {Name}, Location = {Location}, Duartion = {Duration}";
+        }
     }
 }
