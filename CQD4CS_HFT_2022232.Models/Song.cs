381c93d056d816c6bc08bb1b87ed2a59b880d5af
kropfkevin11 <kropfkevin11@gmail.com>
2023-05-16 20:15:27 +0200

    implement song class

--- CQD4CS_HFT_2022232.Models/Song.cs
+++ CQD4CS_HFT_2022232.Models/Song.cs
@@ -1,13 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace CQD4CS_HFT_2022232.Models
 {
     public class Song
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int Lenght { get; set; }
 
+        [ForeignKey(nameof(Artist))]
+        public int ArtistId { get; set; }
+        [NotMapped]
+        public virtual Artist Artist { get; set; }
+
+        public override string ToString()
+        {
+            return $"#{Id}-SONG: Title = {Title}, Genre = {Genre}, Lenght = {Lenght}, ArtistId = {ArtistId}";
+        }
     }
 }
