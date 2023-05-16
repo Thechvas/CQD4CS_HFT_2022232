2b192bb5929715d2a57105428a57ed8c7221711c
kropfkevin11 <kropfkevin11@gmail.com>
2023-05-16 20:06:39 +0200

    implement artist class

--- CQD4CS_HFT_2022232.Models/Artist.cs
+++ CQD4CS_HFT_2022232.Models/Artist.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,27 @@ namespace CQD4CS_HFT_2022232.Models
 {
     public class Artist
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int NumOfAlbums { get; set; }
 
+        [ForeignKey(nameof(Festival))]
+        public int FestivalId { get; set; }
+        [NotMapped]
+        public virtual Festival Festival { get; set; }
+        [NotMapped]
+        public virtual ICollection<Song> Songs { get; set; }
+
+        public Artist()
+        {
+            Songs = new HashSet<Song>();
+        }
+
+        public override string ToString()
+        {
+            return $"#{Id}-ARTIST: Name = {Name}, Number of albums = {NumOfAlbums}, FestivalId = {FestivalId}";
+        }
     }
 }
