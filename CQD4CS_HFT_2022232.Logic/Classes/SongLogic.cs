66de1d5f38d05e1d396d189231f74302b4d36834
kropfkevin11 <kropfkevin11@gmail.com>
2023-05-17 23:12:35 +0200

    create and implement SongLogic class with crud methods

---
+++ CQD4CS_HFT_2022232.Logic/Classes/SongLogic.cs
@@ -0,0 +1,54 @@
+﻿using CQD4CS_HFT_2022232.Logic.Interfaces;
+using CQD4CS_HFT_2022232.Models;
+using CQD4CS_HFT_2022232.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQD4CS_HFT_2022232.Logic.Classes
+{
+    public class SongLogic : ISongLogic
+    {
+        IRepository<Song> repo;
+        public SongLogic(IRepository<Song> repo)
+        {
+            this.repo = repo;
+        }
+
+        public void Create(Song item)
+        {
+            if (item.Lenght < 0)
+            {
+                throw new ArgumentException("Lenght of songs cannot be negative!");
+            }
+            this.repo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.repo.Delete(id);
+        }
+
+        public Song Read(int id)
+        {
+            var song = this.repo.Read(id);
+            if (song == null)
+            {
+                throw new ArgumentException("Song does not exist");
+            }
+            return song;
+        }
+
+        public IQueryable<Song> ReadAll()
+        {
+            return this.repo.ReadAll();
+        }
+
+        public void Update(Song item)
+        {
+            this.repo.Update(item);
+        }
+    }
+}
