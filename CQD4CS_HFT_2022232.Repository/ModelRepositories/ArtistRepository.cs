eb7334b1b8a8448017a96d2c3ea65caf4734695c
kropfkevin11 <kropfkevin11@gmail.com>
2023-05-17 21:09:54 +0200

    create and implement ArtistRepository class

---
+++ CQD4CS_HFT_2022232.Repository/ModelRepositories/ArtistRepository.cs
@@ -0,0 +1,34 @@
+﻿using CQD4CS_HFT_2022232.Models;
+using CQD4CS_HFT_2022232.Repository.Database;
+using CQD4CS_HFT_2022232.Repository.GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQD4CS_HFT_2022232.Repository.ModelRepositories
+{
+    public class ArtistRepository : Repository<Artist>
+    {
+        public ArtistRepository(FestivalDbContext ctx) : base(ctx)
+        {
+
+        }
+
+        public override Artist Read(int id)
+        {
+            return ctx.Artists.FirstOrDefault(a => a.Id == id);
+        }
+
+        public override void Update(Artist item)
+        {
+            var old = Read(item.Id);
+            foreach (var prop in item.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            ctx.SaveChanges();
+        }
+    }
+}
