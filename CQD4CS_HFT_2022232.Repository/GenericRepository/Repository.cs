b858f98cdca54ca8cafffaca99a9d4a97b7d80f1
kropfkevin11 <kropfkevin11@gmail.com>
2023-05-17 20:54:14 +0200

    create and implement generic repository class with crud methods

---
+++ CQD4CS_HFT_2022232.Repository/GenericRepository/Repository.cs
@@ -0,0 +1,44 @@
+﻿using CQD4CS_HFT_2022232.Repository.Database;
+using CQD4CS_HFT_2022232.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQD4CS_HFT_2022232.Repository.GenericRepository
+{
+    public abstract class Repository<T> : IRepository<T> where T : class
+    {
+        protected FestivalDbContext ctx;
+
+        protected Repository(FestivalDbContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public void Create(T item)
+        {
+            ctx.Set<T>().Add(item);
+            ctx.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            ctx.Set<T>().Remove(Read(id));
+            ctx.SaveChanges();
+        }
+
+        public IQueryable<T> ReadAll()
+        {
+            return ctx.Set<T>();
+        }
+
+        public abstract T Read(int id);
+        
+        public abstract void Update(T item);
+
+
+
+    }
+}
