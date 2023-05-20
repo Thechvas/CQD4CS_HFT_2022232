8f2fc5b9005070e5957938cb596db9701d5d0764
kropfkevin11 <kropfkevin11@gmail.com>
2023-05-20 19:09:30 +0200

    Revert "implemented startup class and created controller classes"
    This reverts commit 8dbcf70b7900615a92ec6fd98f81a98bd0ea750a.

--- CQD4CS_HFT_2022232.Endpoint/Startup.cs
+++ CQD4CS_HFT_2022232.Endpoint/Startup.cs
@@ -1,17 +1,8 @@
-using Castle.Core.Configuration;
-using CQD4CS_HFT_2022232.Logic.Classes;
-using CQD4CS_HFT_2022232.Logic.Interfaces;
-using CQD4CS_HFT_2022232.Models;
-using CQD4CS_HFT_2022232.Repository.Database;
-using CQD4CS_HFT_2022232.Repository.Interfaces;
-using CQD4CS_HFT_2022232.Repository.ModelRepositories;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,32 +12,10 @@ namespace CQD4CS_HFT_2022232.Endpoint
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddTransient<FestivalDbContext>();
-
-            services.AddTransient<IRepository<Festival>, FestivalRepository>();
-            services.AddTransient<IRepository<Artist>, ArtistRepository>();
-            services.AddTransient<IRepository<Song>, SongRepository>();
-
-            services.AddTransient<IFestivalLogic, FestivalLogic>();
-            services.AddTransient<IArtistLogic, ArtistLogic>();
-            services.AddTransient<ISongLogic, SongLogic>();
-            
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CQD4CS_HFT_2022232.Endpoint", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,24 +24,16 @@ namespace CQD4CS_HFT_2022232.Endpoint
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CQD4CS_HFT_2022232.Endpoint v1"));
             }
-
-            app.UseExceptionHandler(c => c.Run(async context =>
-            {
-                var exception = context.Features.Get<IExceptionHandlerPathFeature>().Error;
-                var response = new { error = exception.Message };
-                await context.Response.WriteAsJsonAsync(response);
-            }));
 
             app.UseRouting();
 
-            app.UseAuthorization();
-
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
             });
         }
     }
