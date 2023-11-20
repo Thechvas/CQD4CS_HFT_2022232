042609bbcc30270a7c224677b96f85faf4c1a92b
kropfkevin11@stud.uni-obuda.hu <kropfkevin11@stud.uni-obuda.hu>
2023-11-20 15:31:23 +0100

    updated MainWindowViewModel

---
+++ CQD4CS_HFT_2022232.WpfClient/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CQD4CS_HFT_2022232.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQD4CS_HFT_2022232.WpfClient
+{
+    public class MainWindowViewModel
+    {
+        public RestCollection<Artist> Artists { get; set; }
+        public RestCollection<Song> Songs { get; set; }
+        public RestCollection<Festival> Festivals { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Artists = new RestCollection<Artist>("http://localhost:36286/", "artist");
+            Songs = new RestCollection<Song>("http://localhost:36286/", "song");
+            Festivals = new RestCollection<Festival>("http://localhost:36286/", "festival");
+        }
+    }
+}
