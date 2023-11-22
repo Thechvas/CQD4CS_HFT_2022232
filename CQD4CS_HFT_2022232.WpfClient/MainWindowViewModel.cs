72e667ab8054989831ff7855fb5079801794080e
kropfkevin11@stud.uni-obuda.hu <kropfkevin11@stud.uni-obuda.hu>
2023-11-23 00:46:13 +0100

    non cruds added

--- CQD4CS_HFT_2022232.WpfClient/MainWindowViewModel.cs
+++ CQD4CS_HFT_2022232.WpfClient/MainWindowViewModel.cs
@@ -3,6 +3,7 @@ using Microsoft.Toolkit.Mvvm.ComponentModel;
 using Microsoft.Toolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,23 @@ namespace CQD4CS_HFT_2022232.WpfClient
         public RestCollection<Artist> Artists { get; set; }
         public RestCollection<Song> Songs { get; set; }
         public RestCollection<Festival> Festivals { get; set; }
+        public RestService rest {  get; set; }
+
+        private string longestSongOfArtist;
+        public string LongestSongOfArtist
+        {
+            get { return longestSongOfArtist; }
+            set
+            {
+                if (longestSongOfArtist != value)
+                {
+                    longestSongOfArtist = value;
+                    OnPropertyChanged(nameof(LongestSongOfArtist));
+                }
+            }
+        }
+
+        public string FestivalWithMostArtists { get; set; }
 
         private Artist selectedArtist;
 
@@ -97,11 +115,31 @@ namespace CQD4CS_HFT_2022232.WpfClient
         public ICommand DeleteFestivalCommand { get; set; }
         public ICommand UpdateFestivalCommand { get; set; }
 
+        public ICommand LongestSongOfArtistCommand { get; set; }
+
+
+        public string getLongestSongOfArtist(string artistName)
+        {
+
+            LongestSongOfArtist = rest.GetSingle<string>($"Stat/LongestSongOfArtist?artistName={artistName}");
+            
+            return LongestSongOfArtist;
+        }
+
         public MainWindowViewModel()
         {
+            rest = new RestService("http://localhost:36286/");
             Artists = new RestCollection<Artist>("http://localhost:36286/", "artist", "hub");
             Songs = new RestCollection<Song>("http://localhost:36286/", "song", "hub");
             Festivals = new RestCollection<Festival>("http://localhost:36286/", "festival", "hub");
+            FestivalWithMostArtists = new RestService("http://localhost:36286/").GetSingle<string>("Stat/FestivalWithMostArtists");
+            //if (SelectedArtist != null)
+            //{
+            //    LongestSongOfArtist = new RestService("http://localhost:36286/")
+            //        .GetSingle<string>($"Stat/LongestSongOfArtist?artistName={SelectedArtist.Name}");
+            //}
+
+
 
             CreateArtistCommand = new RelayCommand(() =>
             {
@@ -174,11 +212,22 @@ namespace CQD4CS_HFT_2022232.WpfClient
                 return SelectedFestival != null;
             });
 
+            LongestSongOfArtistCommand = new RelayCommand(() =>
+            {
+                getLongestSongOfArtist(SelectedArtist.Name);
+                
+            },
+            () =>
+            {
+                return SelectedArtist != null;
+
+            });
+
 
             SelectedArtist = new Artist();
             SelectedSong = new Song();
             SelectedFestival = new Festival(3);
-      
+
         }
     }
 }
