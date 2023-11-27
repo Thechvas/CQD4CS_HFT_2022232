2b6484f97c293c54027e68634f9585b4a65955bb
kropfkevin11@stud.uni-obuda.hu <kropfkevin11@stud.uni-obuda.hu>
2023-11-27 23:55:57 +0100

    delete added

--- CQD4CS_HFT_2022232.JSClient/wwwroot/script.js
+++ CQD4CS_HFT_2022232.JSClient/wwwroot/script.js
@@ -1,24 +1,41 @@
-﻿let actors = [];
+﻿let artists = [];
 getdata();
 
 async function getdata() {
     await fetch('http://localhost:36286/artist')
         .then(x => x.json())
         .then(y => {
-            actors = y;
-            console.log(actors);
+            artists = y;
+            console.log(artists);
             display();
         });
 }
 
+
 function display() {
-    actors.forEach(t => {
+    document.getElementById('resultarea').innerHTML = "";
+    artists.forEach(t => {
         document.getElementById('resultarea').innerHTML +=
             "<tr><td>" + t.id + "</td><td>"
-            + t.name + "</td></tr>";
-
-        console.log(t.name);
+            + t.name + "</td><td>" +
+            `<button type="button" onclick="remove(${t.id})">Delete</button>`
+            + "</td></tr>";
     });
+}
+
+
+function remove(id){
+    fetch('http://localhost:36286/artist/' + id, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json', },
+        body: null
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
 }
 
 function create() {
@@ -27,13 +44,10 @@ function create() {
         method: 'POST',
         headers: { 'Content-Type': 'application/json', },
         body: JSON.stringify(
-            {
-                artistname: name,
-
-            })
-    })
+            { name: name })})
         .then(response => response)
-        .then(data => {
+        .then(data =>
+        {
             console.log('Success:', data);
             getdata();
         })
