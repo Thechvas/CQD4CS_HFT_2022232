55a01c507aa3d8528bc11e23d046ad720e466797
kropfkevin11@stud.uni-obuda.hu <kropfkevin11@stud.uni-obuda.hu>
2023-11-27 15:55:52 +0100

    create added

--- CQD4CS_HFT_2022232.JSClient/wwwroot/script.js
+++ CQD4CS_HFT_2022232.JSClient/wwwroot/script.js
@@ -1,12 +1,15 @@
 ﻿let actors = [];
+getdata();
 
-fetch('http://localhost:36286/artist')
-    .then(x => x.json())
-    .then(y => {
-        actors = y;
-        console.log(actors);
-        display();
-    });
+async function getdata() {
+    await fetch('http://localhost:36286/artist')
+        .then(x => x.json())
+        .then(y => {
+            actors = y;
+            console.log(actors);
+            display();
+        });
+}
 
 function display() {
     actors.forEach(t => {
@@ -16,4 +19,23 @@ function display() {
 
         console.log(t.name);
     });
+}
+
+function create() {
+    let name = document.getElementById('artistname').value;
+    fetch('http://localhost:36286/artist', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify(
+            {
+                artistname: name,
+
+            })
+    })
+        .then(response => response)
+        .then(data => {
+            console.log('Success:', data);
+            getdata();
+        })
+        .catch((error) => { console.error('Error:', error); });
 }
